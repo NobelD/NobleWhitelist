0d9f48fbbc5ca05283e2d9cc8b4cd3f26c8dfedb
NobelD <nobeldavidmiu@hotmail.com>
2024-06-25 15:23:06 -0500

    Adventure Relocate

--- nwl-spigot/src/main/java/me/nobeld/noblewhitelist/util/BukkitAdventure.java
+++ nwl-spigot/src/main/java/me/nobeld/noblewhitelist/util/BukkitAdventure.java
@@ -11,13 +11,16 @@ import org.bukkit.entity.Player;
 
 public class BukkitAdventure implements AdvPlatformManager {
     private final NobleWhitelist data;
+
     public BukkitAdventure(NobleWhitelist data) {
         this.data = data;
     }
+
     @Override
     public void startAdventure() {
         data.setProvider(BukkitAudiences.create(data));
     }
+
     @Override
     public void closeAdventure() {
         if (data.getProvider() != null) {
@@ -26,19 +29,34 @@ public class BukkitAdventure implements AdvPlatformManager {
             data.setProvider(null);
         }
     }
+
     @Override
     public Audience consoleAudience() {
         return data.getProvider().console();
     }
+
     @Override
     public Audience playerAudience(Object player) {
         return data.getProvider().player((Player) player);
     }
+
     @Override
     public Audience senderAudience(Object sender) {
         return data.getProvider().sender((CommandSender) sender);
     }
+
     public Audience senderAudienceSp(CommandSender sender) {
         return senderAudience(sender);
+    }
+
+    private static final LegacyComponentSerializer legacySerializer = LegacyComponentSerializer.builder()
+            .character('\u00a7')
+            .hexColors()
+            .extractUrls()
+            .useUnusualXRepeatedCharacterHexFormat()
+            .build();
+
+    public static String asLegacy(Component component) {
+        return legacySerializer.serialize(component);
     }
 }
