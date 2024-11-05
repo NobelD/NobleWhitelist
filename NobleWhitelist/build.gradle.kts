1deda7c0f72924fb32c5a5529bb79f69278c34cc
NobelD <nobeldavidmiu@hotmail.com>
2024-11-05 13:47:03 -0500

    1.2.5

--- NobleWhitelist/build.gradle.kts
+++ NobleWhitelist/build.gradle.kts
@@ -36,7 +36,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper", "paper-api", "1.20.4-R0.1-SNAPSHOT")
     implementation("io.papermc", "paperlib", "1.0.7")
-    implementation("com.alessiodp.libby", "libby-bukkit", "2.0.0-20240104.190327-5") {
+    implementation("com.alessiodp.libby", "libby-bukkit", "2.0.0-20240104.190327-5") { // TODO fix libby version
         exclude(module=("spigot-api"))
     }
     compileOnly("com.github.simplix-softworks","simplixstorage","3.2.6")
