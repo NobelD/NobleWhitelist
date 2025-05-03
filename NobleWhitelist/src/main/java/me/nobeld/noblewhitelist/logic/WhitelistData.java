d39b3127b4e4208519f09b8677ec073acc5f7465
NobelD <nobeldavidmiu@hotmail.com>
2025-05-03 11:23:22 -0500

    Fix checking order

--- NobleWhitelist/src/main/java/me/nobeld/noblewhitelist/logic/WhitelistData.java
+++ NobleWhitelist/src/main/java/me/nobeld/noblewhitelist/logic/WhitelistData.java
@@ -24,8 +24,8 @@ public class WhitelistData {
      */
     public Optional<WhitelistEntry> getEntry(@Nullable String name, @Nullable UUID uuid, @Range(from=-1, to=Long.MAX_VALUE) long id) {
         WhitelistEntry data = null;
-        if (name != null) data = this.data.getStorage().loadPlayer(name);
-        if (data == null && uuid != null) data = this.data.getStorage().loadPlayer(uuid);
+        if (uuid != null) data = this.data.getStorage().loadPlayer(uuid);
+        if (data == null && name != null) data = this.data.getStorage().loadPlayer(name);
         if (data == null && id >= 0) data = this.data.getStorage().loadPlayer(id);
 
         return Optional.ofNullable(data);
